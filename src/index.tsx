0af2999edfaf1481e3395f7bfbdedfb9782c0dfa
elon <yilong_zhang@windmagics.com>
2023-05-07 22:58:50 +0800

    remove console.log

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import ColorPicker from "./ColorPicker";
-console.log(ColorPicker.name)
 ColorPicker.install = function (app) {
-  console.log(ColorPicker.name)
   app.component(ColorPicker.name, ColorPicker);
   return app;
 };
